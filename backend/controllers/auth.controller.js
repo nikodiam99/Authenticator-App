a50a10a836f6c539293ab91c256c1aa6c11cc26c
nikodiam99 <nikodiam99@gmail.com>
2024-02-18 21:27:57 -0500

    Created signup api route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+import User from '../models/user.models.js';
+import bcryptjs from 'bcryptjs';
+//request is data we are getting from client side
+//response is data we send to client side
+export const signup = async (req, res) => {
+    //console.log(req.body);
+    const{username, email, password} = req.body;
+    //hashSync instead of hash that is asyncronous
+    const hashedPassword = bcryptjs.hashSync(password, 10); 
+    //created from user model
+    const newUser = new User({username, email, password: hashedPassword});
+    try{
+        //we need to use await because it is asynchronous behaviour
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully."});
+    } catch (error){
+        res.status(500).json(error.message);
+    }
+    //we need to use await because it is asynchronous behaviour
+    
+};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 //place mongo connection in env file to hide password and connection to DB
 //and check if connection is successfull 
@@ -11,9 +13,12 @@ mongoose.connect(process.env.MONGO).then (()=>{
     console.log(err);
 });
 const app = express();
+//so we can send json as input of backend
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
 
 app.use("/backend/user", userRoutes);
+app.use("/backend/auth", authRoutes);
