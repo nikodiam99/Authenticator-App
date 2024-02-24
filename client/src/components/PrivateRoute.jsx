299c9475c1631d8642d0c7296ba3c19590b3cdbd
nikodiam99 <nikodiam99@gmail.com>
2024-02-24 15:01:13 -0500

    completed profile page UI

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -4,6 +4,7 @@ import { Outlet, Navigate} from 'react-router-dom'
 
 export default function privateRoute() {
     const {currentUser} = useSelector(state => state.user)
+    //render children if there is no current user, otherwise go to sign in page
   return currentUser ? <Outlet/> : <Navigate to = '/sign-in'/>
     
   
