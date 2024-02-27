d267e6cafadbf33bbca0c639f75671ea584b84ab
nikodiam99 <nikodiam99@gmail.com>
2024-02-26 22:57:05 -0500

    completed about and homepage

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-sky-700'>About</h1>
+    <p className='mb-4 text-sky-900'>This is a MERN (MongoDB, Express, React, Node.js) stack application with 
+    authentication. It allows users to sign up, log in, and log out, delete & update account, 
+    and provides access to protected routes only for authenticated 
+    users.</p>
+  </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-sky-700'>Welcome to the Authentication app</h1>
+      <p className='mb-4 text-sky-900'>This is my first full-stack web application built with the MERN 
+        (MongoDB, Express, React, Node.js) stack. 
+        It includes authentication features that allow users to 
+        sign up, log in, log out, and provides access to 
+        protected routes only for authenticated users.</p>
+      <p className='mb-4 text-sky-900'>The front-end of the application is built with React and uses 
+      React Router for client-side routing. The back-end is built with 
+      Node.js and Express, and uses MongoDB as the database. 
+      Authentication is implemented using JSON Web Tokens (JWT).</p>
+    </div>
   )
 }
